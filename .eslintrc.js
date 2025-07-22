39132fb2419b490fc82048f3b4bf4a61aea6e484
ContentMark <contentmark.ai@gmail.com>
2025-07-22 11:44:35 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,54 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended'
+  ],
   env: {
     node: true,
     es2020: true,
     jest: true
   },
-  extends: [
-    'eslint:recommended'
-  ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module'
+    sourceType: 'module',
+    project: null // Disable type-checking for faster CI
   },
   rules: {
-    // Basic JavaScript rules
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_'
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    
+    // General rules
     'no-console': 'off', // CLI tool needs console output
-    'no-debugger': 'error',
     'no-var': 'error',
     'prefer-const': 'error',
-    'prefer-arrow-callback': 'error',
-    'arrow-spacing': 'error',
-    'no-duplicate-imports': 'error',
-    'no-useless-return': 'error',
-    'prefer-template': 'error',
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
     'comma-dangle': ['error', 'never'],
     'indent': ['error', 2],
     'no-trailing-spaces': 'error',
-    'eol-last': 'error'
+    'eol-last': 'error',
+    
+    // Disable base rule in favor of TypeScript version
+    'no-unused-vars': 'off'
   },
   overrides: [
     {
-      // TypeScript files - use basic parsing without TypeScript-specific rules
-      files: ['**/*.ts'],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-          // Allow TypeScript syntax
-        }
-      },
-      rules: {
-        // Disable rules that don't work well with TypeScript syntax
-        'no-undef': 'off', // TypeScript handles this
-        'no-unused-vars': ['error', { 
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_'
-        }]
-      }
-    },
-    {
-      // Test files
-      files: ['**/*.test.ts', '**/*.spec.ts', '**/tests/**/*.ts'],
+      files: ['**/*.test.ts', '**/*.spec.ts'],
       env: {
         jest: true
       },
       rules: {
-        'no-console': 'off',
-        'max-len': 'off'
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'off'
       }
     }
   ],
